01373dd8b9028d98379a19732648b73753e9156c
unta1337 <firstapple42@gmail.com>
2022-11-28 09:49:35 +0900

    열거형 형식 수정

--- State-VendingMachine/src/State.java
+++ State-VendingMachine/src/State.java
@@ -1,113 +1,141 @@
+/**
+ * @copyright 한국기술교육대학교 컴퓨터공학부 객체지향개발론및실습
+ * @version 2022년도 2학기
+ * @author 김성녕
+ * 상태 패턴
+ * 자동 판매기 상태
+ */
 public enum State {
-    COIN_EMPTY,
-    COIN_INSERTED,
-    ITEM_EMPTY,
-    ITEM_SOLD;
-
-    public void insertCash(VendingMachine vendingMachine, Currency currency, int amount) {
-        switch (this) {
-            case COIN_EMPTY:
-                System.out.println("Insert a coin into the machine.");
-                
-                // 동전 삽입 후 대기 상태(동전이 삽입되어 상품 선택을 기다리는 상태)로 전환.
-                vendingMachine.addCash(currency, amount);
-                vendingMachine.setState(COIN_INSERTED);
-                break;
-            case COIN_INSERTED:
-                System.out.println("Insert a coin into the machine.");
-
-                // 추가 동전 삽입.
-                vendingMachine.addCash(currency, amount);
-                break;
-            case ITEM_EMPTY:
-                System.out.println("Vending machine is empty.");
-                break;
-            case ITEM_SOLD:
-                System.out.println("Product is on its way! Please wait for a second.");
-                break;
+    COIN_EMPTY {
+        @Override
+        public void insertCash(VendingMachine vendingMachine, Currency currency, int amount) {
+            System.out.println("Insert a coin into the machine.");
+            
+            // 동전 삽입 후 대기 상태(동전이 삽입되어 상품 선택을 기다리는 상태)로 전환.
+            vendingMachine.addCash(currency, amount);
+            vendingMachine.setState(COIN_INSERTED);
         }
-    }
 
-    public void selectItem(VendingMachine vendingMachine, Item item) throws ChangeNotAvailableException {
-        switch (this) {
-            case COIN_EMPTY:
-                System.out.println("Please insert a coin first.");
-                break;
-            case COIN_INSERTED:
-                System.out.println("You've seleted " + item.name().toLowerCase() + ".");
-
-                // 상품 구매할 수 없을 때 처리.
-                if (!vendingMachine.canBuyItem(item)) {
-                    // 다른 구매 가능한 상품이 없으면 거스름 반환.
-                    if (!vendingMachine.canBuyAnyItem())
-                        vendingMachine.returnChange();
-
-                    // 거스름 관련 예외 발생.
-                    throw new ChangeNotAvailableException(!vendingMachine.canBuyAnyItem());
-                }
-
-                vendingMachine.setState(ITEM_SOLD);
-                break;
-            case ITEM_EMPTY:
-                System.out.println("Vending machine is empty.");
-                break;
-            case ITEM_SOLD:
-                System.out.println("Product is on its way! Please wait for a second.");
-                break;
+        @Override
+        public void selectItem(VendingMachine vendingMachine, Item item) throws ChangeNotAvailableException {
+            System.out.println("Please insert a coin first.");
         }
-    }
 
-    public void cancel(VendingMachine vendingMachine) {
-        switch (this) {
-            case COIN_EMPTY:
-                System.out.println("There's nothing to cancel.");
-                break;
-            case COIN_INSERTED:
-                System.out.println("Item selection canceled.");
-
-                // 상품 구매 취소 시 거스름 반환 및 상태 전환.
-                vendingMachine.returnChange();
-                vendingMachine.setState(COIN_EMPTY);
-                break;
-            case ITEM_EMPTY:
-                System.out.println("Vending machine is empty.");
-                break;
-            case ITEM_SOLD:
-                System.out.println("You've reached the point of no return. Just wait for the consequences.");
-                break;
+        @Override
+        public void cancel(VendingMachine vendingMachine) {
+            System.out.println("There's nothing to cancel.");
         }
-    }
 
-    public void dispenseItem(VendingMachine vendingMachine, Item item) {
-        switch (this) {
-            case COIN_EMPTY:
-                System.out.println("Please insert a coin first.");
-                break;
-            case COIN_INSERTED:
-                System.out.println("Please select a item first.");
-                break;
-            case ITEM_EMPTY:
-                System.out.println("Vending machine is empty.");
-                break;
-            case ITEM_SOLD:
-                System.out.println("Product is on its way! Please wait for a second.");
+        @Override
+        public void dispenseItem(VendingMachine vendingMachine, Item item) {
+            System.out.println("Please insert a coin first.");
+        }
+    },
 
-                // 상품 배출.
-                vendingMachine.removeItem(item);
+    COIN_INSERTED {
+        @Override
+        public void insertCash(VendingMachine vendingMachine, Currency currency, int amount) {
+            System.out.println("Insert a coin into the machine.");
 
-                // 거스름 처리.
-                CashRegister newUserCashRegister = vendingMachine.getChange(vendingMachine.getInsertedBalance() - item.price);
-                vendingMachine.setUserCashRegister(newUserCashRegister);
+            // 추가 동전 삽입.
+            vendingMachine.addCash(currency, amount);
+        }
 
-                // 더 이상 구매 가능한 상품이 없으면 상태 전환.
-                if (!vendingMachine.canBuyAnyItem()) {
+        @Override
+        public void selectItem(VendingMachine vendingMachine, Item item) throws ChangeNotAvailableException {
+            System.out.println("You've seleted " + item.name().toLowerCase() + ".");
+
+            // 상품 구매할 수 없을 때 처리.
+            if (!vendingMachine.canBuyItem(item)) {
+                // 다른 구매 가능한 상품이 없으면 거스름 반환.
+                if (!vendingMachine.canBuyAnyItem())
                     vendingMachine.returnChange();
-                    return;
-                }
 
-                // 계속해서 구매 가능하면 동전 삽입 상태로 전환.
-                vendingMachine.setState(COIN_INSERTED);
-                break;
+                // 거스름 관련 예외 발생.
+                throw new ChangeNotAvailableException(!vendingMachine.canBuyAnyItem());
+            }
+
+            vendingMachine.setState(ITEM_SOLD);
         }
-    }
+
+        @Override
+        public void cancel(VendingMachine vendingMachine) {
+            System.out.println("Item selection canceled.");
+
+            // 상품 구매 취소 시 거스름 반환 및 상태 전환.
+            vendingMachine.returnChange();
+            vendingMachine.setState(COIN_EMPTY);
+        }
+
+        @Override
+        public void dispenseItem(VendingMachine vendingMachine, Item item) {
+            System.out.println("Please select a item first.");
+        }
+    },
+
+    ITEM_EMPTY {
+        @Override
+        public void insertCash(VendingMachine vendingMachine, Currency currency, int amount) {
+            System.out.println("Vending machine is empty.");
+        }
+
+        @Override
+        public void selectItem(VendingMachine vendingMachine, Item item) throws ChangeNotAvailableException {
+            System.out.println("Vending machine is empty.");
+        }
+
+        @Override
+        public void cancel(VendingMachine vendingMachine) {
+            System.out.println("Vending machine is empty.");
+        }
+
+        @Override
+        public void dispenseItem(VendingMachine vendingMachine, Item item) {
+            System.out.println("Vending machine is empty.");
+        }
+    },
+
+    ITEM_SOLD {
+        @Override
+        public void insertCash(VendingMachine vendingMachine, Currency currency, int amount) {
+            System.out.println("Product is on its way! Please wait for a second.");
+        }
+
+        @Override
+        public void selectItem(VendingMachine vendingMachine, Item item) throws ChangeNotAvailableException {
+            System.out.println("Product is on its way! Please wait for a second.");
+
+        }
+
+        @Override
+        public void cancel(VendingMachine vendingMachine) {
+            System.out.println("You've reached the point of no return. Just wait for the consequences.");
+        }
+
+        @Override
+        public void dispenseItem(VendingMachine vendingMachine, Item item) {
+            System.out.println("Product is on its way! Please wait for a second.");
+
+            // 상품 배출.
+            vendingMachine.removeItem(item);
+
+            // 거스름 처리.
+            CashRegister newUserCashRegister = vendingMachine.getChange(vendingMachine.getInsertedBalance() - item.price);
+            vendingMachine.setUserCashRegister(newUserCashRegister);
+
+            // 더 이상 구매 가능한 상품이 없으면 상태 전환.
+            if (!vendingMachine.canBuyAnyItem()) {
+                vendingMachine.returnChange();
+                return;
+            }
+
+            // 계속해서 구매 가능하면 동전 삽입 상태로 전환.
+            vendingMachine.setState(COIN_INSERTED);
+        }
+    };
+
+    public abstract void insertCash(VendingMachine vendingMachine, Currency currency, int amount);
+    public abstract void selectItem(VendingMachine vendingMachine, Item item) throws ChangeNotAvailableException;
+    public abstract void cancel(VendingMachine vendingMachine);
+    public abstract void dispenseItem(VendingMachine vendingMachine, Item item);
 }
